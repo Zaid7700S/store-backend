976c26859f985127f3754e09aab18f35b47bcff0
Zaid7700S <zaid3055540@gmail.com>
2026-07-26 15:43:16 +0500

    Add Email property to CreateUserDto

--- Dto/CreateUserDto.cs
+++ Dto/CreateUserDto.cs
@@ -4,6 +4,7 @@
     {
         public string? Name { get; set; } = string.Empty;
         public string? userName { get; set; } = string.Empty;
+        public string Email { get; set; }
         public string? password { get; set; } = string.Empty;
         public string? Role { get; set; } = string.Empty;
     }
