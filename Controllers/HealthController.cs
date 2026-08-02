b2b6f33f9f366fdc7587e37dc89540b85d4aa97d
Muhammad Zaid <zaid3055540@gmail.com>
2026-08-02 11:57:46 +0500

    Add HealthController for health check endpoint

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+[ApiController]
+[Route("health")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new
+        {
+            status = "healthy",
+            timestamp = DateTime.UtcNow
+        });
+    }
+}
